ed2dd90c21203744b377953bcb605d33110df0b6
MegaYEye <xcx1106@126.com>
2021-05-10 00:30:14 -0400

    .

---
+++ whisker-PC/pywhisker.py
@@ -0,0 +1,12 @@
+import numpy as np
+import serial
+
+class Sensor(object):
+    def __init__(self):
+        pass
+    
+    def start_communication(self):
+        pass
+    
+    def read_value(self):
+        return self.values
